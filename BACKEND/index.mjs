62a31b4c4403b60e067c9a7c9a3773161b782d76
NichoStrick <nicoparedes00@gmail.com>
2025-09-19 17:36:13 -0300

    Back modularizado y funcionando - 19/09

--- BACKEND/index.mjs
+++ BACKEND/index.mjs
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
-import rutasApi from './modulos/rutas.mjs';
+import rutasApiTurnos from './modulos/turnos/rutas.turno.mjs';
+import rutasApiServicios from './modulos/servicios/rutas.servicio.mjs';
 import bodyParser from 'body-parser'; 
 import cors from 'cors';
 
@@ -12,7 +13,8 @@ app.use(bodyParser.json());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(rutasApi);
+app.use(rutasApiTurnos);
+app.use(rutasApiServicios);
 
 app.listen(PUERTO, () => {
     console.log(`Servidor escuchando en el puerto ${PUERTO}`);
